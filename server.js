260166bfe7366cbb07c1d1c9c3ed0b69977cc1f9
James Wang <jw@slickage.com>
2014-03-03 15:01:24 -1000

    Foreman standard config.

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var util = require('util');
 
 // global env + requires
-var env = process.env.NODE_ENV || 'development';
 var httpServer = require('./http_server');
-var config = require('./' + env + '.config.json');
+var config = require('./config');
 
 util.log('route_view-api');
 util.log(util.inspect(config, true, null));
 
-var port = process.env.HTTP_PORT || config.port;
-httpServer.listen(port, function() {
-  util.log('HTTP server listening on port ' + port);
+httpServer.listen(config.port, function() {
+  util.log('HTTP server listening on port ' + config.port);
 });
 
